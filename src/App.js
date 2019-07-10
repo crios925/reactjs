ee60ba8fa955ddee0c7e5cf54acd1fbe2aa386e3
crios925 <cristhian.rios@dafiti.com.co>
2019-07-10 15:19:18 -0500

    agregar cambios de estados y props

--- src/App.js
+++ src/App.js
@@ -1,28 +1,49 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import ProductDetail from "./components/productDetail";
+import PropTypes from "prop-types";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <ProductDetail productTitle={"Zapatillas"} price={1000} size={41}/>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const data ={
+  productTitle: 'Zapatillas calidosas',
+  price: 1200,
+  size: 41,
+  priceT: "PRICE",
 }
+
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      data:data,
+      category: 'masculino'
+    }
+  }
+  updateClick = () =>{
+    this.setState({category:"hola"})
+  }
+  render (){
+    return (
+        <div className="App">
+          <header className="App-header">
+            <h1>{this.state.category}</h1>
+            <ProductDetail data={data}/>
+            <img src={logo} className="App-logo" alt="logo" />
+            <button onClick={this.updateClick}>Actualizar</button>
+          </header>
+        </div>
+    );
+  }
+
+}
+
+ProductDetail.PropTypes = {
+
+  data: PropTypes.shape({
+    productTitle: PropTypes.string.isRequired,
+    price: PropTypes.number.isRequired
+  })
+
+};
 
 export default App;
--- src/components/productDetail/index.js
+++ src/components/productDetail/index.js
@@ -1,16 +1,14 @@
 import React from 'react';
 
+const ProductDetail = ({data:{productTitle,size,price, priceT}}) =>{
 
-
-const ProductDetail = ({productTitle, size}) =>{
     return(
         <div>
             <h1>{productTitle}</h1>
             <span>{size}</span>
         </div>
     )
-}
-
+};
 
 
 export default ProductDetail;
